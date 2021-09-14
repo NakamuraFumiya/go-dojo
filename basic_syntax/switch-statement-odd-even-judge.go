c3023718851c9ed9ff79d977da77b6a1285c6271
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2021-09-14 19:44:07 +0900

    switch文を使った、奇数か偶数を判定して出力するプログラムを作成

--- basic_syntax/switch-statement-odd-even-judge.go
+++ basic_syntax/switch-statement-odd-even-judge.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		if i%2 == 0 {
+		switch {
+		case i%2 == 0:
 			fmt.Printf("%d-偶数\n", i)
-		} else {
+		default:
 			fmt.Printf("%d-奇数\n", i)
 		}
 	}
